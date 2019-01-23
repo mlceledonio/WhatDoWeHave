601ed307d64d25f95e7e21a944daad08efd77924
Messiah Celedonio <celedoniomessiah@gmail.com>
2019-01-23 21:04:45 +1100

    Added a list view to show temporary list and added a floating action button to add items

--- app/src/main/java/com/mesha/whatdowehave/MainActivity.kt
+++ app/src/main/java/com/mesha/whatdowehave/MainActivity.kt
@@ -2,11 +2,22 @@ package com.mesha.whatdowehave
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById<ListView>(R.id.items_list_view)
+        var itemList = listOf("corned beef", "century tuna", "maling");
+
+        val adapter = ArrayAdapter(this, R.layout.simple_item_list, itemList)
+        listView.adapter = adapter
+
     }
 }
